7330e3ddb02f8774494118322fd1af2e2c934221
Legend <milad1995honor@gmail.com>
2021-03-28 13:57:40 +0430

    init data store

---
+++ store/data.js
@@ -0,0 +1,12 @@
+export const state = () => ({
+	totalOrders: 2736,
+    averageShipments: 364,
+    inventory: 400,
+    trendingGifts: [
+        { src: '/p-1.png', alt: 'gift 1', name: 'gift name', url: '#' },
+        { src: '/p-2.png', alt: 'gift 2', name: 'gift name', url: '#' },
+        { src: '/p-3.png', alt: 'gift 3', name: 'gift name', url: '#' },
+        { src: '/collection.png', alt: 'gift collection', name: 'collection name', url: '#' }
+    ],
+    totalGiftBarChartData: []
+})
