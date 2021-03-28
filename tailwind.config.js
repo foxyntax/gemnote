124138eb6785fc6ead015e71a0440bea8ce66b2a
Legend <milad1995honor@gmail.com>
2021-03-28 13:59:19 +0430

    config tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+	future: {
+		// removeDeprecatedGapUtilities: true,
+		// purgeLayersByDefault: true,
+	},
+	purge: {
+		enabled: process.env.NODE_ENV === 'production',
+		content: [
+			'components/*.vue',
+			'layouts/*.vue',
+			'pages/*.vue',
+			'pages/**/*.vue',
+			'plugins/*.js',
+			'nuxt.config.js'
+		]
+	},
+	corePlugins: {
+		ringWidth: false
+	},
+	theme: {},
+	variants: {},
+	plugins: []
+}
